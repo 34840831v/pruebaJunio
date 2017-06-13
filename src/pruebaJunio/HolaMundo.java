d24221d118a3432e2483e1dcdd953455b9d3fe42
Javier <Javier@LAPTOP-8Q3H0GFR>
2017-06-13 20:17:09 +0200

    creo clase HolaMundo

---
+++ src/pruebaJunio/HolaMundo.java
@@ -0,0 +1,13 @@
+package pruebaJunio;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+		
+	}
+}
